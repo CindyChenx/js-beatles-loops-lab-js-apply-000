95d6f6391611ace2746fcb5528b3a97c4757eae3
outstanding-bolt-9487 <chen_cx0128@hotmail.com>
2019-05-21 01:07:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,23 +19,23 @@ var facts = [
   "He hated the sound of his own voice"
 ];
 
-// function johnLennonFacts(facts){
-// var x = [ ];
-// var i = 0;
-// while (i < facts.length){
-//     x[i] = facts[i] + "!!!";
-// }
-//   return x;
-// }
+function johnLennonFacts(facts){
+var x = [ ];
+var i = 0;
+while (i < facts.length){
+    x[i] = facts[i] + "!!!";
+}
+  return x;
+}
 
-// var n = 0;
-// function iLoveTheBeatles(n){
-//   var x = [ ];
-//   do{
-//     x[n] = "I love the Beatles!";
-//     n++;
-//   }while(n<15);
-// }
+var n = 0;
+function iLoveTheBeatles(n){
+  var x = [ ];
+  do{
+    x[n] = "I love the Beatles!";
+    n++;
+  }while(n<15);
+}
 
 
 
